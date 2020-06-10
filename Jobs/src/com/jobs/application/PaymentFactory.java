12c2bb1cc27cd50512e92d9a39e2fba6805b710d
Wicka <mesaes@gmail.com>
2020-06-10 21:09:07 +0200

    Metodo Manager

--- Jobs/src/com/jobs/application/PaymentFactory.java
+++ Jobs/src/com/jobs/application/PaymentFactory.java
@@ -2,7 +2,7 @@ package com.jobs.application;
 
 import com.jobs.domain.IPaymentRate;
 
-public class PaymentFactory {
+public class PaymentFactory implements IPaymentRate{
 
 	
 	public static IPaymentRate createPaymentRateBoss(){
@@ -21,5 +21,20 @@ public class PaymentFactory {
 				return 0;//todo 
 			}
 		};
+	}
+	// METODO PARA MANAGER
+	
+	public static IPaymentRate createPaymentRateManager(){
+		return new IPaymentRate() {
+			@Override
+			public double pay(double salaryPerMonth) {
+				return 0;//todo
+			}
+		};
+	}
+
+	@Override
+	public double pay(double salaryPerMonth) {
+		return 0;
 	}
 }
